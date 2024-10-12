981aed4a0e766e848786fc57879137d7e3d1bc9c
shimeoki <shimeoki@gmail.com>
2024-10-12 10:37:03 +0300

    initial commit

---
+++ lib/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    `java-library`
+    id("org.openjfx.javafxplugin") version "0.1.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+javafx {
+    version = "21"
+    modules("javafx.controls", "javafx.fxml")
+    configuration = "implementation"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
