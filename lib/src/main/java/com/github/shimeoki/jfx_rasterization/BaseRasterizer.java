bd2720a279093533b4e34780c0f132dd57fdbcb0
shimeoki <shimeoki@gmail.com>
2024-10-15 14:10:03 +0300

    docs(BaseRasterizer): add javadoc

--- lib/src/main/java/com/github/shimeoki/jfx_rasterization/BaseRasterizer.java
+++ lib/src/main/java/com/github/shimeoki/jfx_rasterization/BaseRasterizer.java
@@ -2,6 +2,10 @@ package com.github.shimeoki.jfx_rasterization;
 
 import javafx.scene.canvas.GraphicsContext;
 
+/**
+ * <code>BaseRasterizer</code> represents a blank rasterizer with cached
+ * <code>GraphicsContext</code>.
+ */
 public interface BaseRasterizer {
 
     public GraphicsContext getCtx();
