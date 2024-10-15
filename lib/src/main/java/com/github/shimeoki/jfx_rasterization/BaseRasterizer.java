e9f24fa25dfa743c6f3820b5f2304266e3e34a0a
shimeoki <shimeoki@gmail.com>
2024-10-15 13:30:05 +0300

    feat: remove Color from BaseRasterizer

--- lib/src/main/java/com/github/shimeoki/jfx_rasterization/BaseRasterizer.java
+++ lib/src/main/java/com/github/shimeoki/jfx_rasterization/BaseRasterizer.java
@@ -1,15 +1,10 @@
 package com.github.shimeoki.jfx_rasterization;
 
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
 
 public interface BaseRasterizer {
 
     public GraphicsContext getCtx();
 
     public void setCtx(GraphicsContext ctx);
-
-    public Color getColor();
-
-    public void setColor(Color color);
 }
