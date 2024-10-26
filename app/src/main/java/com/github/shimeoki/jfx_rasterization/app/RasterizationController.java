8bb4212ed7ab09c3ab0c2a2f724caf7ad00b01e9
shimeoki <shimeoki@gmail.com>
2024-10-26 21:22:42 +0300

    feat(app): remove canvas and rename anchorPane

--- app/src/main/java/com/github/shimeoki/jfx_rasterization/app/RasterizationController.java
+++ app/src/main/java/com/github/shimeoki/jfx_rasterization/app/RasterizationController.java
@@ -1,16 +1,12 @@
 package com.github.shimeoki.jfx_rasterization.app;
 
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.AnchorPane;
 
 public class RasterizationController {
 
     @FXML
-    private AnchorPane anchorPane;
-
-    @FXML
-    private Canvas canvas;
+    private AnchorPane root;
 
     @FXML
     private void initialize() {
