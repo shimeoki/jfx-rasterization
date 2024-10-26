984e005260c936d1fc76e9d999580412bf12ecd6
shimeoki <shimeoki@gmail.com>
2024-10-26 13:01:04 +0300

    build: add working app configuration
    1. move resources into correct directory
    2. not launch app as a module

--- app/src/main/java/module-info.java
+++ app/src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.github.shimeoki.jfx_rasterization.app {
 
     requires com.github.shimeoki.jfx_rasterization.lib;
 
-    requires javafx.graphics;
+    requires transitive javafx.graphics;
     requires javafx.fxml;
 
     exports com.github.shimeoki.jfx_rasterization.app;
