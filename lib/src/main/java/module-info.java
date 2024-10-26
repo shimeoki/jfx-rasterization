beab4be142093255abfd06d484cec7d7371e70ea
shimeoki <shimeoki@gmail.com>
2024-10-26 11:37:58 +0300

    build(lib): export all packages in module-info

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -3,4 +3,6 @@ module com.github.shimeoki.jfx_rasterization.lib {
     requires transitive javafx.graphics;
 
     exports com.github.shimeoki.jfx_rasterization.lib;
+    exports com.github.shimeoki.jfx_rasterization.lib.colors;
+    exports com.github.shimeoki.jfx_rasterization.lib.rasterizers;
 }
