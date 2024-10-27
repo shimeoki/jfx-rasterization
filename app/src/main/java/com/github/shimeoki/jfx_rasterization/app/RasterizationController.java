b9123b6120e871bcf8b238ad018e3260b0f25755
shimeoki <shimeoki@gmail.com>
2024-10-27 19:02:49 +0300

    feat(RasterizationController): add Tile subclass

--- app/src/main/java/com/github/shimeoki/jfx_rasterization/app/RasterizationController.java
+++ app/src/main/java/com/github/shimeoki/jfx_rasterization/app/RasterizationController.java
@@ -1,6 +1,16 @@
 package com.github.shimeoki.jfx_rasterization.app;
 
+import java.util.Objects;
+import java.util.Random;
+
+import com.github.shimeoki.jfx_rasterization.lib.TriangleColor;
+import com.github.shimeoki.jfx_rasterization.lib.TriangleRasterizer;
+import com.github.shimeoki.jfx_rasterization.lib.rasterizers.StandardTriangleRasterizer;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.TilePane;
 
@@ -15,6 +25,71 @@ public class RasterizationController {
     @FXML
     private void initialize() {
         initTiles();
+    }
+
+    class Tile {
+
+        private final Canvas c;
+        private final TriangleRasterizer r;
+
+        // 6 numbers from 0 to 1
+        // 2 coordinates for 3 points
+        // represent % of length in the canvas
+        private final double[] pos = new double[6];
+
+        private final Random rnd = new Random();
+
+        Tile(final Canvas canvas, final TriangleColor color) {
+            Objects.requireNonNull(canvas);
+            Objects.requireNonNull(color);
+
+            this.c = canvas;
+
+            this.r = new StandardTriangleRasterizer();
+            r.setColor(color);
+            r.setCtx(c.getGraphicsContext2D());
+
+            generate();
+        }
+
+        private Point2D getPoint1() {
+            final double x = c.getWidth() * pos[0];
+            final double y = c.getHeight() * pos[1];
+            return new Point2D(x, y);
+        }
+
+        private Point2D getPoint2() {
+            final double x = c.getWidth() * pos[2];
+            final double y = c.getHeight() * pos[3];
+            return new Point2D(x, y);
+        }
+
+        private Point2D getPoint3() {
+            final double x = c.getWidth() * pos[4];
+            final double y = c.getHeight() * pos[5];
+            return new Point2D(x, y);
+        }
+
+        void generate() {
+            for (int i = 0; i < pos.length; i++) {
+                pos[i] = rnd.nextDouble();
+            }
+        }
+
+        void clear() {
+            final GraphicsContext ctx = c.getGraphicsContext2D();
+            ctx.clearRect(0, 0, c.getWidth(), c.getHeight());
+        }
+
+        void draw() {
+            clear();
+
+            final Point2D p1 = getPoint1();
+            final Point2D p2 = getPoint2();
+            final Point2D p3 = getPoint3();
+
+            r.draw(p1, p2, p3);
+        }
     }
 
     private void initTiles() {
