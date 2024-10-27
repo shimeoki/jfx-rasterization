178de6631d968989ee3e74a524b5a0b2f8e4288f
shimeoki <shimeoki@gmail.com>
2024-10-27 18:52:39 +0300

    feat(app): add tiles

--- app/src/main/java/com/github/shimeoki/jfx_rasterization/app/RasterizationController.java
+++ app/src/main/java/com/github/shimeoki/jfx_rasterization/app/RasterizationController.java
@@ -2,6 +2,7 @@ package com.github.shimeoki.jfx_rasterization.app;
 
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.TilePane;
 
 public class RasterizationController {
 
@@ -9,6 +10,15 @@ public class RasterizationController {
     private AnchorPane root;
 
     @FXML
+    private TilePane tiles;
+
+    @FXML
     private void initialize() {
+        initTiles();
+    }
+
+    private void initTiles() {
+        tiles.setPrefRows(3);
+        tiles.setPrefColumns(3);
     }
 }
