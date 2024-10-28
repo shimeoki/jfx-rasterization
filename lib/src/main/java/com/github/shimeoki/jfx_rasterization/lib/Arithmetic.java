27c0f1b34c195d5e51544c8d777b52f92ea452b7
shimeoki <shimeoki@gmail.com>
2024-10-29 01:55:38 +0300

    feat(lib): add Arithmetic class

---
+++ lib/src/main/java/com/github/shimeoki/jfx_rasterization/lib/Arithmetic.java
@@ -0,0 +1,18 @@
+package com.github.shimeoki.jfx_rasterization.lib;
+
+public class Arithmetic {
+
+    public static final double EPSILON = 0.000000000001;
+
+    public static boolean equals(final double v1, final double v2) {
+        return Math.abs(v1 - v2) < EPSILON;
+    }
+
+    public static boolean moreThan(final double left, final double right) {
+        return (left - right) > -EPSILON;
+    }
+
+    public static boolean lessThan(final double left, final double right) {
+        return (left - right) < EPSILON;
+    }
+}
