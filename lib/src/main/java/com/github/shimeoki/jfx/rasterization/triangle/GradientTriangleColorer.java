a158e736d35f3fa2b46e9bba1a6f2c77845ef05e
shimeoki <shimeoki@gmail.com>
2024-10-29 18:56:37 +0300

    refactor(GradientTriangleColorer): make more concise

--- lib/src/main/java/com/github/shimeoki/jfx/rasterization/triangle/GradientTriangleColorer.java
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/triangle/GradientTriangleColorer.java
@@ -10,9 +10,9 @@ public class GradientTriangleColorer implements TriangleColorer {
     public static final Color DEFAULT_COLOR_2 = Color.FORESTGREEN;
     public static final Color DEFAULT_COLOR_3 = Color.BLUE;
 
-    private Color color1 = DEFAULT_COLOR_1;
-    private Color color2 = DEFAULT_COLOR_2;
-    private Color color3 = DEFAULT_COLOR_3;
+    private Color c1 = DEFAULT_COLOR_1;
+    private Color c2 = DEFAULT_COLOR_2;
+    private Color c3 = DEFAULT_COLOR_3;
 
     public GradientTriangleColorer() {
     }
@@ -26,83 +26,75 @@ public class GradientTriangleColorer implements TriangleColorer {
     }
 
     public Color getColor1() {
-        return color1;
+        return c1;
     }
 
     public Color getColor2() {
-        return color2;
+        return c2;
     }
 
     public Color getColor3() {
-        return color3;
+        return c3;
     }
 
-    public void setColor1(final Color color) {
-        Objects.requireNonNull(color);
+    public void setColor1(final Color c) {
+        Objects.requireNonNull(c);
 
-        this.color1 = color;
+        c1 = c;
     }
 
-    public void setColor2(final Color color) {
-        Objects.requireNonNull(color);
+    public void setColor2(final Color c) {
+        Objects.requireNonNull(c);
 
-        this.color2 = color;
+        c2 = c;
     }
 
-    public void setColor3(final Color color) {
-        Objects.requireNonNull(color);
+    public void setColor3(final Color c) {
+        Objects.requireNonNull(c);
 
-        this.color3 = color;
+        c3 = c;
     }
 
-    private double getRed(final TriangleBarycentrics b) {
-        final double r1 = b.lambda1() * getColor1().getRed();
-        final double r2 = b.lambda2() * getColor2().getRed();
-        final double r3 = b.lambda3() * getColor3().getRed();
-
-        final double red = r1 + r2 + r3;
+    private double red(final TriangleBarycentrics b) {
+        final double r1 = b.lambda1() * c1.getRed();
+        final double r2 = b.lambda2() * c2.getRed();
+        final double r3 = b.lambda3() * c3.getRed();
 
         // should be in [0.0, 1.0]
-        return Math.min(Math.max(0, red), 1);
+        return Math.min(Math.max(0, r1 + r2 + r3), 1);
     }
 
-    private double getGreen(final TriangleBarycentrics b) {
-        final double g1 = b.lambda1() * getColor1().getGreen();
-        final double g2 = b.lambda2() * getColor2().getGreen();
-        final double g3 = b.lambda3() * getColor3().getGreen();
-
-        final double green = g1 + g2 + g3;
+    private double green(final TriangleBarycentrics b) {
+        final double g1 = b.lambda1() * c1.getGreen();
+        final double g2 = b.lambda2() * c2.getGreen();
+        final double g3 = b.lambda3() * c3.getGreen();
 
         // should be in [0.0, 1.0]
-        return Math.min(Math.max(0, green), 1);
+        return Math.min(Math.max(0, g1 + g2 + g3), 1);
     }
 
-    private double getBlue(final TriangleBarycentrics b) {
-        final double b1 = b.lambda1() * getColor1().getBlue();
-        final double b2 = b.lambda2() * getColor2().getBlue();
-        final double b3 = b.lambda3() * getColor3().getBlue();
-
-        final double blue = b1 + b2 + b3;
+    private double blue(final TriangleBarycentrics b) {
+        final double b1 = b.lambda1() * c1.getBlue();
+        final double b2 = b.lambda2() * c2.getBlue();
+        final double b3 = b.lambda3() * c3.getBlue();
 
         // should be in [0.0, 1.0]
-        return Math.min(Math.max(0, blue), 1);
+        return Math.min(Math.max(0, b1 + b2 + b3), 1);
     }
 
-    private double getOpacity(final TriangleBarycentrics b) {
-        final double o1 = b.lambda1() * getColor1().getOpacity();
-        final double o2 = b.lambda2() * getColor2().getOpacity();
-        final double o3 = b.lambda3() * getColor3().getOpacity();
-
-        final double opacity = o1 + o2 + o3;
+    private double opacity(final TriangleBarycentrics b) {
+        final double o1 = b.lambda1() * c1.getOpacity();
+        final double o2 = b.lambda2() * c2.getOpacity();
+        final double o3 = b.lambda3() * c3.getOpacity();
 
         // should be in [0.0, 1.0]
-        return Math.min(Math.max(0, opacity), 1);
+        return Math.min(Math.max(0, o1 + o2 + o3), 1);
     }
 
     @Override
     public Color get(final TriangleBarycentrics b) {
         Objects.requireNonNull(b);
 
-        return new Color(getRed(b), getGreen(b), getBlue(b), getOpacity(b));
+        return new Color(red(b), green(b), blue(b), opacity(b));
     }
 }
