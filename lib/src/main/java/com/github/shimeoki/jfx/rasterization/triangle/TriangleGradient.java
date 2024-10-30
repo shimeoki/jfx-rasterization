074463089c3d486c04c9ddb09b1845bb0117953c
shimeoki <shimeoki@gmail.com>
2024-10-30 17:29:39 +0300

    feat: add TriangleGradient

---
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/triangle/TriangleGradient.java
@@ -0,0 +1,36 @@
+package com.github.shimeoki.jfx.rasterization.triangle;
+
+import com.github.shimeoki.jfx.rasterization.Colorf;
+
+public interface TriangleGradient {
+
+    // first color
+
+    public Colorf color1();
+
+    public float red1();
+
+    public float green1();
+
+    public float blue1();
+
+    // second color
+
+    public Colorf color2();
+
+    public float red2();
+
+    public float green2();
+
+    public float blue2();
+
+    // third color
+
+    public Colorf color3();
+
+    public float red3();
+
+    public float green3();
+
+    public float blue3();
+}
