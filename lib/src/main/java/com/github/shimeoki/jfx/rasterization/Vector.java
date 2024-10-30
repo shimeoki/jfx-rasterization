5a0b75fa4b2091f54f11616183aeeab86a0f81a1
shimeoki <shimeoki@gmail.com>
2024-10-30 16:03:55 +0300

    feat: add Vector

---
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/Vector.java
@@ -0,0 +1,32 @@
+package com.github.shimeoki.jfx.rasterization;
+
+public class Vector implements Vector2D {
+
+    private float x;
+    private float y;
+
+    public Vector(final float x, final float y) {
+        setX(x);
+        setY(y);
+    }
+
+    @Override
+    public float x() {
+        return x;
+    }
+
+    @Override
+    public void setX(final float x) {
+        this.x = x;
+    }
+
+    @Override
+    public float y() {
+        return y;
+    }
+
+    @Override
+    public void setY(final float y) {
+        this.y = y;
+    }
+}
