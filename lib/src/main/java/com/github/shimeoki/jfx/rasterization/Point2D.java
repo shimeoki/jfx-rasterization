2010355575d7542ad81cb210c441bcc8a85b18f3
shimeoki <shimeoki@gmail.com>
2024-10-30 15:57:58 +0300

    feat: add Point2D and Vector2D

---
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/Point2D.java
@@ -0,0 +1,8 @@
+package com.github.shimeoki.jfx.rasterization;
+
+public interface Point2D {
+
+    public float x();
+
+    public float y();
+}
---
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/Vector2D.java
@@ -0,0 +1,8 @@
+package com.github.shimeoki.jfx.rasterization;
+
+public interface Vector2D extends Point2D {
+
+    public void setX(final float x);
+
+    public void setY(final float y);
+}
