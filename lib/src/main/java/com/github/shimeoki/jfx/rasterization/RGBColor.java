0e977abfa8e31682cca73eb7df02085721f069f2
shimeoki <shimeoki@gmail.com>
2024-10-30 17:41:05 +0300

    feat: add RGBColor

---
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/RGBColor.java
@@ -0,0 +1,50 @@
+package com.github.shimeoki.jfx.rasterization;
+
+import javafx.scene.paint.Color;
+
+public class RGBColor implements Colorf {
+
+    private final float r;
+    private final float g;
+    private final float b;
+    private final float a;
+
+    public RGBColor(
+            final float r, final float g, final float b, final float a) {
+
+        this.r = FloatMath.confined(0, r, 1);
+        this.g = FloatMath.confined(0, g, 1);
+        this.b = FloatMath.confined(0, b, 1);
+        this.a = FloatMath.confined(0, a, 1);
+    }
+
+    @Override
+    public float red() {
+        return r;
+    }
+
+    @Override
+    public float green() {
+        return g;
+    }
+
+    @Override
+    public float blue() {
+        return b;
+    }
+
+    @Override
+    public float alpha() {
+        return a;
+    }
+
+    @Override
+    public float opacity() {
+        return a;
+    }
+
+    @Override
+    public Color jfxColor() {
+        return new Color(r, g, b, a);
+    }
+}
