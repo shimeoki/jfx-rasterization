a33ed6913bd6c0b6aca377d932ae7b0bf8441dcc
shimeoki <shimeoki@gmail.com>
2024-10-31 22:15:31 +0300

    feat: add HTMLColors

---
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/color/HTMLColors.java
@@ -0,0 +1,54 @@
+package com.github.shimeoki.jfx.rasterization.color;
+
+public class HTMLColors {
+
+    // docs: https://htmlcolorcodes.com/color-names/
+
+    // red
+
+    public static final Colorf RED = new RGBColor(1f, 0f, 0f, 1f);
+
+    // pink
+
+    // ...
+
+    // orange
+
+    public static final Colorf ORANGE = new RGBColor(1f, 0.65f, 0f, 1f);
+
+    // yellow
+
+    public static final Colorf YELLOW = new RGBColor(1f, 1f, 0f, 1f);
+
+    // purple
+
+    public static final Colorf FUCHSIA = new RGBColor(1f, 0f, 1f, 1f);
+
+    public static final Colorf PURPLE = new RGBColor(0.5f, 0f, 0.5f, 1f);
+
+    // green
+
+    public static final Colorf LIME = new RGBColor(0f, 0.5f, 0f, 1f);
+
+    public static final Colorf GREEN = new RGBColor(0f, 1f, 0f, 1f);
+
+    // blue
+
+    public static final Colorf AQUA = new RGBColor(0f, 1f, 1f, 1f);
+
+    public static final Colorf CYAN = new RGBColor(0f, 1f, 1f, 1f);
+
+    public static final Colorf BLUE = new RGBColor(0f, 0f, 1f, 1f);
+
+    // brown
+
+    public static final Colorf MAROON = new RGBColor(0.5f, 0f, 0f, 1f);
+
+    // white
+
+    public static final Colorf WHITE = new RGBColor(1f, 1f, 1f, 1f);
+
+    // gray
+
+    public static final Colorf BLACK = new RGBColor(0f, 0f, 0f, 1f);
+}
