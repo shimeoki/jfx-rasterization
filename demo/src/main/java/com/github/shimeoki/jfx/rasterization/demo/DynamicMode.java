68407a17fe748ba9b3aabd8a25634cad301f5539
shimeoki <shimeoki@gmail.com>
2024-11-02 10:41:41 +0300

    feat(demo/dynamic): add triangle generation

--- demo/src/main/java/com/github/shimeoki/jfx/rasterization/demo/DynamicMode.java
+++ demo/src/main/java/com/github/shimeoki/jfx/rasterization/demo/DynamicMode.java
@@ -2,11 +2,15 @@ package com.github.shimeoki.jfx.rasterization.demo;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 import com.github.shimeoki.jfx.rasterization.color.HTMLColors;
+import com.github.shimeoki.jfx.rasterization.geom.Vector;
+import com.github.shimeoki.jfx.rasterization.geom.Vector2D;
 import com.github.shimeoki.jfx.rasterization.triangle.DDATriangler;
 import com.github.shimeoki.jfx.rasterization.triangle.Triangler;
 import com.github.shimeoki.jfx.rasterization.triangle.color.MonotoneTriangleColorer;
+import com.github.shimeoki.jfx.rasterization.triangle.geom.DynamicTriangle;
 import com.github.shimeoki.jfx.rasterization.triangle.geom.Triangle;
 
 import javafx.fxml.FXML;
@@ -31,6 +35,10 @@ public class DynamicMode {
 
     @FXML
     private Button clearBtn;
+
+    private final Random rnd = new Random();
+
+    private final List<Vector2D> vertices = new LinkedList<>();
 
     private final List<Triangle> triangles = new LinkedList<>();
 
@@ -76,6 +84,27 @@ public class DynamicMode {
                         0,
                         canvas.getWidth(),
                         canvas.getHeight());
+    }
+
+    private Vector2D randomVector() {
+        final float x = rnd.nextFloat((float) canvas.getWidth());
+        final float y = rnd.nextFloat((float) canvas.getHeight());
+
+        return new Vector(x, y);
+    }
+
+    private void addTriangle() {
+        final Vector2D v1 = randomVector();
+        final Vector2D v2 = randomVector();
+        final Vector2D v3 = randomVector();
+
+        final Triangle t = new DynamicTriangle(v1, v2, v3);
+
+        vertices.add(v1);
+        vertices.add(v2);
+        vertices.add(v3);
+
+        triangles.add(t);
     }
 
     private void draw() {
