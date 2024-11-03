e24dec68bc4a85e0c7024d7c9333582192962f19
shimeoki <shimeoki@gmail.com>
2024-11-03 16:42:38 +0300

    style(GradientTriangleColorer): variable names

--- lib/src/main/java/com/github/shimeoki/jfx/rasterization/triangle/color/GradientTriangleColorer.java
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/triangle/color/GradientTriangleColorer.java
@@ -9,42 +9,42 @@ import com.github.shimeoki.jfx.rasterization.triangle.geom.TriangleBarycentrics;
 
 public class GradientTriangleColorer implements TriangleColorer {
 
-    private final TriangleGradient g;
+    private final TriangleGradient gradient;
 
     public GradientTriangleColorer(final TriangleGradient g) {
         Objects.requireNonNull(g);
 
-        this.g = g;
+        gradient = g;
     }
 
     private float red(final TriangleBarycentrics b) {
-        final float r1 = b.lambda1() * g.red1();
-        final float r2 = b.lambda2() * g.red2();
-        final float r3 = b.lambda3() * g.red3();
+        final float r1 = b.lambda1() * gradient.red1();
+        final float r2 = b.lambda2() * gradient.red2();
+        final float r3 = b.lambda3() * gradient.red3();
 
         return Floats.confined(0, r1 + r2 + r3, 1);
     }
 
     private float green(final TriangleBarycentrics b) {
-        final float g1 = b.lambda1() * g.green1();
-        final float g2 = b.lambda2() * g.green2();
-        final float g3 = b.lambda3() * g.green3();
+        final float g1 = b.lambda1() * gradient.green1();
+        final float g2 = b.lambda2() * gradient.green2();
+        final float g3 = b.lambda3() * gradient.green3();
 
         return Floats.confined(0, g1 + g2 + g3, 1);
     }
 
     private float blue(final TriangleBarycentrics b) {
-        final float b1 = b.lambda1() * g.blue1();
-        final float b2 = b.lambda2() * g.blue2();
-        final float b3 = b.lambda3() * g.blue3();
+        final float b1 = b.lambda1() * gradient.blue1();
+        final float b2 = b.lambda2() * gradient.blue2();
+        final float b3 = b.lambda3() * gradient.blue3();
 
         return Floats.confined(0, b1 + b2 + b3, 1);
     }
 
     private float opacity(final TriangleBarycentrics b) {
-        final float o1 = b.lambda1() * g.alpha1();
-        final float o2 = b.lambda2() * g.alpha2();
-        final float o3 = b.lambda3() * g.alpha3();
+        final float o1 = b.lambda1() * gradient.alpha1();
+        final float o2 = b.lambda2() * gradient.alpha2();
+        final float o3 = b.lambda3() * gradient.alpha3();
 
         return Floats.confined(0, o1 + o2 + o3, 1);
     }
