90466fa032d56528c58079ad1aae38017da3ff4d
shimeoki <shimeoki@gmail.com>
2024-11-04 18:01:28 +0300

    docs(TriangleColorer): remove javadoc

--- lib/src/main/java/com/github/shimeoki/jfx/rasterization/triangle/color/TriangleColorer.java
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/triangle/color/TriangleColorer.java
@@ -3,23 +3,8 @@ package com.github.shimeoki.jfx.rasterization.triangle.color;
 import com.github.shimeoki.jfx.rasterization.color.Colorf;
 import com.github.shimeoki.jfx.rasterization.triangle.geom.TriangleBarycentrics;
 
-/**
- * A functional interface for {@code TriangleRasterizer} to get colors for
- * individual pixels.
- *
- * @see {@code TriangleRasterizer}
- */
 @FunctionalInterface
 public interface TriangleColorer {
 
-    /**
-     * Gets a {@code Colorf} at specified barycentic coordinates.
-     *
-     * @param b barycentric coordinates.
-     * 
-     * @return {@code Colorf} at specified barycentric coordinates.
-     * 
-     * @throws {@code NullPointerException} if b is null.
-     */
     public Colorf get(final TriangleBarycentrics b);
 }
