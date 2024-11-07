5a570dbd85716e2ce0941c8c5c0349ce364c4e29
shimeoki <shimeoki@gmail.com>
2024-11-08 00:06:11 +0300

    docs(Pos2f): add javadoc

--- lib/src/main/java/com/github/shimeoki/jfx/rasterization/geom/Pos2f.java
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/geom/Pos2f.java
@@ -1,8 +1,29 @@
 package com.github.shimeoki.jfx.rasterization.geom;
 
+/**
+ * Interface that represents a static position in 2D space using floats.
+ *
+ * <p>
+ * Because the implementation can be dynamic ({@code x} and {@code y} are not
+ * constants), it acts just as a minimal interface for retrieving coordinates,
+ * and not guarentees the static behaviour.
+ *
+ * @author shimeoki
+ * @since 1.0.0
+ */
 public interface Pos2f {
 
+    /**
+     * Gets the first coordinate of the position.
+     *
+     * @return x (first) coordinate
+     */
     public float x();
 
+    /**
+     * Gets the second coordinate of the position.
+     *
+     * @return y (second) coordinate
+     */
     public float y();
 }
