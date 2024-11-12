9c10c2a2781495fa09a163f33a83bc00f9543a1f
shimeoki <shimeoki@gmail.com>
2024-11-12 14:47:08 +0300

    docs(Triangler): add javadoc

--- lib/src/main/java/com/github/shimeoki/jfx/rasterization/triangle/Triangler.java
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/triangle/Triangler.java
@@ -5,9 +5,34 @@ import com.github.shimeoki.jfx.rasterization.triangle.geom.Triangle;
 
 import javafx.scene.canvas.GraphicsContext;
 
+/**
+ * A functional interface for triangle rasterizers.
+ *
+ * <p>
+ * Accepts {@link GraphicsContext GraphicsContext} from a JavaFX {@code Canvas}
+ * to draw the pixels, {@link Triangle Triangle} as the shape to draw and
+ * {@link TriangleColorer TriangleColorer} to get the colors for individual
+ * pixels.
+ *
+ * @author shimeoki
+ * @since 1.0.0
+ *
+ * @see GraphicsContext
+ * @see Triangle
+ * @see TriangleColorer
+ */
 @FunctionalInterface
 public interface Triangler {
 
+    /**
+     * Draws the {@code triangle} colored by {@code colorer} with {@code ctx}.
+     *
+     * @param ctx      the {@link GraphicsContext GraphicsContext} to draw with
+     * @param triangle the {@link Triangle Triangle} to draw
+     * @param colorer  the color mapping {@link TriangleColorer TriangleColorer}
+     *
+     * @since 1.0.0
+     */
     public void draw(
             final GraphicsContext ctx,
             final Triangle triangle,
