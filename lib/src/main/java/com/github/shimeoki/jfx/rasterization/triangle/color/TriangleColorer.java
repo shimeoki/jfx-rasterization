6a624e6914721c3a974acbe12b9d718111c6596d
shimeoki <shimeoki@gmail.com>
2024-11-16 10:48:01 +0300

    docs(TriangleColorer): add javadoc

--- lib/src/main/java/com/github/shimeoki/jfx/rasterization/triangle/color/TriangleColorer.java
+++ lib/src/main/java/com/github/shimeoki/jfx/rasterization/triangle/color/TriangleColorer.java
@@ -3,8 +3,33 @@ package com.github.shimeoki.jfx.rasterization.triangle.color;
 import com.github.shimeoki.jfx.rasterization.color.Colorf;
 import com.github.shimeoki.jfx.rasterization.triangle.geom.TriangleBarycentrics;
 
+/**
+ * An interface that represents a function to get the colors to fill the
+ * triangle.
+ *
+ * <p>
+ * Can be used for filling triangles with one color, gradients or textures.
+ * <p>
+ * Standard implementations for "monotone" and gradient fill can be found in the
+ * library. You can use them as examples for creating your own class/function.
+ *
+ * @since 1.0.0
+ *
+ * @see StaticMonotoneTriangleColorer
+ * @see DynamicMonotoneTriangleColorer
+ * @see StaticGradientTriangleColorer
+ * @see DynamicGradientTriangleColorer
+ */
 @FunctionalInterface
 public interface TriangleColorer {
 
+    /**
+     * Gets the color for the point on specified barycentrics.
+     *
+     * @param b the barycentric coordinates
+     * @return color for this point
+     *
+     * @since 1.0.0
+     */
     public Colorf get(final TriangleBarycentrics b);
 }
