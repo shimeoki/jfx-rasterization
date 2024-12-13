0ec0bd842004d305a2c4e06e43c89eee5df6ba3a
shimeoki <shimeoki@gmail.com>
2024-12-13 23:22:20 +0300

    docs(color): javadoc

--- lib/src/main/java/io/github/shimeoki/jfx/rasterization/color/package-info.java
+++ lib/src/main/java/io/github/shimeoki/jfx/rasterization/color/package-info.java
@@ -1,6 +1,7 @@
 /**
- * Package for shared color types, mostly the Colorf.
+ * Package for shared color types. Mostly the
+ * {@link io.github.shimeoki.jfx.rasterization.color.Colorf}.
  *
- * @since 1.0.0
+ * @since 2.0.0
  */
 package io.github.shimeoki.jfx.rasterization.color;
