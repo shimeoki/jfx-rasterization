4ad1dba876053edd39e259fd6aa93051ba35b554
shimeoki <shimeoki@gmail.com>
2024-12-14 00:46:38 +0300

    docs(triangle/geom): javadoc

--- lib/src/main/java/io/github/shimeoki/jfx/rasterization/triangle/geom/package-info.java
+++ lib/src/main/java/io/github/shimeoki/jfx/rasterization/triangle/geom/package-info.java
@@ -1,7 +1,7 @@
 /**
- * Package for all triangle related geometry, mostly the triangle type itself
+ * Package for all triangle related geometry. Mostly the triangle type itself
  * and barycentric coordinates.
  *
- * @since 1.0.0
+ * @since 2.0.0
  */
 package io.github.shimeoki.jfx.rasterization.triangle.geom;
