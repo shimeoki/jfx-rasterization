e0710dda4dc726f0d1c8d70dc55d726d36056349
shimeoki <shimeoki@gmail.com>
2024-12-13 23:23:58 +0300

    docs(Point2f): javadoc

--- lib/src/main/java/io/github/shimeoki/jfx/rasterization/geom/Point2f.java
+++ lib/src/main/java/io/github/shimeoki/jfx/rasterization/geom/Point2f.java
@@ -1,45 +1,45 @@
 package io.github.shimeoki.jfx.rasterization.geom;
 
 /**
- * Interface that represents a dynamic point in 2D space using floats.
+ * An interface that represents a point in 2D space using floats.
  *
- * @since 1.0.0
+ * @since 2.0.0
  */
 public interface Point2f {
 
     /**
      * Gets the first coordinate of the position.
      *
-     * @return x (first) coordinate
+     * @return x (first) float coordinate
      *
-     * @since 1.0.0
+     * @since 2.0.0
      */
     public float x();
 
     /**
      * Sets the value of the first coordinate.
      *
-     * @param x new value of x (first) coordinate
+     * @param x new value of x (first) coordinate as a float
      *
-     * @since 1.0.0
+     * @since 2.0.0
      */
     public void setX(final float x);
 
     /**
      * Gets the second coordinate of the position.
      *
-     * @return y (second) coordinate
+     * @return y (second) float coordinate
      *
-     * @since 1.0.0
+     * @since 2.0.0
      */
     public float y();
 
     /**
      * Sets the value of the second coordinate.
      *
-     * @param y new value of y (second) coordinate
+     * @param y new value of y (second) coordinate as a float
      *
-     * @since 1.0.0
+     * @since 2.0.0
      */
     public void setY(final float y);
 }
