64b2d19a210d0c00f3b1f0667b43c44c23743931
shimeoki <shimeoki@gmail.com>
2024-12-14 08:00:05 +0300

    docs(MonotoneTriangleFiller): javadoc

--- lib/src/main/java/io/github/shimeoki/jfx/rasterization/triangle/color/MonotoneTriangleFiller.java
+++ lib/src/main/java/io/github/shimeoki/jfx/rasterization/triangle/color/MonotoneTriangleFiller.java
@@ -5,10 +5,32 @@ import java.util.Objects;
 import io.github.shimeoki.jfx.rasterization.color.Colorf;
 import io.github.shimeoki.jfx.rasterization.triangle.geom.TriangleBarycentrics;
 
+/**
+ * One of the standard implementations of {@link TriangleFiller} to fill the
+ * triangles with one color.
+ * 
+ * <p>
+ * Accepts a {@link Colorf} on creation and fills the triangles with the color
+ * if barycentrics object is not null.
+ * 
+ * @since 2.0.0
+ *
+ * @see TriangleFiller
+ * @see Colorf
+ */
 public final class MonotoneTriangleFiller implements TriangleFiller {
 
     private final Colorf color;
 
+    /**
+     * Creates a new {@link MonotoneTriangleFiller} instance.
+     *
+     * @param c the color to use
+     *
+     * @throws NullPointerException if {@code c} is {@code null}
+     *
+     * @since 2.0.0
+     */
     public MonotoneTriangleFiller(final Colorf c) {
         color = Objects.requireNonNull(c);
     }
