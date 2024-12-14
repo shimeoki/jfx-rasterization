86be34019d0865894ea1a8488f429af96d1bbbf8
shimeoki <shimeoki@gmail.com>
2024-12-14 10:11:48 +0300

    chore: bump version to 2.0.0

--- lib/src/main/java/module-info.java
+++ lib/src/main/java/module-info.java
@@ -1,18 +1,16 @@
 /**
  * A JavaFX shape rasterization library.
  *
- * @version 1.0.1
+ * @version 2.0.0
  */
 module io.github.shimeoki.jfx.rasterization {
 
     requires transitive javafx.graphics;
 
-    // shared
     exports io.github.shimeoki.jfx.rasterization.math;
     exports io.github.shimeoki.jfx.rasterization.geom;
     exports io.github.shimeoki.jfx.rasterization.color;
 
-    // triangle
     exports io.github.shimeoki.jfx.rasterization.triangle;
     exports io.github.shimeoki.jfx.rasterization.triangle.geom;
     exports io.github.shimeoki.jfx.rasterization.triangle.color;
